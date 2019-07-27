189e5a2387ccd4da335a6c11a2eb7931da440333
Gabe <41751617+glstewart17@users.noreply.github.com>
2019-07-27 16:03:39 -0400

    Reformat and remove unnecessary files

--- main.py
+++ main.py
@@ -2,18 +2,16 @@ import os
 import string
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
+from summary import getSummary, train_markov, getArticleText, handleSummary
 
 app = Flask(__name__)
 
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
 
-    # Recieve incomeing message and set to lower
-    body = request.values.get('Body', None).lower()
-
-    # Remove punctuation
-    body.translate(None, string.punctuation)
-
+    # Recieve incomeing message, set to lower, and remove punctuation
+    body = request.values.get('Body', None).lower().translate(str.maketrans('','',"""!#$%&'()"*+,./:;<=>?@[]^_`{|}~"""))
+    
     # Start response
     resp = MessagingResponse()
     
@@ -22,11 +20,13 @@ def incoming_sms():
     farewells = ['bye', 'goodbye', "farewell", 'see ya']
     
     if body == 'help':
-        resp.message("Hi friend! Text 'help' for instructions")
+        resp.message("Type any subject, can be a word or phrase, and we will send you a brief summary.")
     elif body in greetings:
         resp.message("Hi friend! Text 'help' for instructions")
     elif body in farewells:
         resp.message("See you soon!")
+    else:
+        resp.message(handleSummary(body))
 
     return str(resp)
 
